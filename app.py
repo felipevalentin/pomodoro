f5b8955233352ca808c61356effb93915b12121d
Felipe Valentin Nascimento <gitfelipevn@gmail.com>
2021-11-07 02:05:45 -0300

    Refactor changed methods and atributes order to a more organized one

--- app.py
+++ app.py
@@ -8,28 +8,28 @@ import threading
 class Pomodoro:
     def __init__(self, master):
         self.master = master
-        self.window_config()
-
         self.frame = tk.Frame(self.master)
+
+        # Timer
         self.time_thread = threading.Thread()
         self.time = 25*60
         self.time_string = tk.StringVar()
-        self.update_time(self.time)
+        self.run = True
 
-        self.time_label = tk.Entry(self.frame, textvariable=self.time_string, font=(None, 40,), width=0)
+        # Buttons
         self.focus_button = tk.Button(self.frame, text = 'Focus', command=self.focus, width=17)
-        self.short_break_button = tk.Button(self.frame, text = 'Short Break', command=self.short_break, width=17)
         self.long_break_button = tk.Button(self.frame, text = 'Long Break', command=self.long_break, width=17)
+        self.reset_button = tk.Button(self.frame, text = 'reset', command = self.reset, width=17)
+        self.short_break_button = tk.Button(self.frame, text = 'Short Break', command=self.short_break, width=17)
         self.start_button = tk.Button(self.frame, text = 'start', command = self.start, width=17)
         self.stop_button = tk.Button(self.frame, text = 'stop', command = self.stop, width=17)
-        self.reset_button = tk.Button(self.frame, text = 'reset', command = self.reset, width=17)
+
+        # Labels
+        self.time_label = tk.Entry(self.frame, textvariable=self.time_string, font=(None, 40,), width=0)
+
         self.display_on_window()
-        self.run = True
-    
-    def window_config(self):
-        self.master.title("Pomodoro")
-        self.master.geometry("500x185")
-        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.window_config()
+        self.update_time(self.time)
 
     def display_on_window(self):
         self.time_label.grid(row=1, column=1, pady=25)
@@ -40,6 +40,33 @@ class Pomodoro:
         self.stop_button.grid(row=2, column=1)
         self.reset_button.grid(row=2, column=2)
         self.frame.pack()
+
+    def window_config(self):
+        self.master.title("Pomodoro")
+        self.master.geometry("500x185")
+        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def focus(self):
+        self.update_time(25*60)
+        self.start()
+
+    def long_break(self):
+        self.update_time(10*60)
+        self.start()
+
+    def on_closing(self):
+        if self.time_thread.is_alive():
+            self.run = False
+            self.time_thread.join()
+        self.master.destroy()
+
+    def reset(self):
+        self.stop()
+        self.update_time(25*60)
+
+    def short_break(self):
+        self.update_time(5*60)
+        self.start()
 
     def start(self):
         if self.time_thread.is_alive():
@@ -52,10 +79,6 @@ class Pomodoro:
     def stop(self):
         self.run = False
 
-    def reset(self):
-        self.stop()
-        self.update_time(25*60)
-
     def timer(self):
         while self.run and self.time > 0:
             self.update_time(self.time - 1)
@@ -63,12 +86,6 @@ class Pomodoro:
         if self.time == 0:
             self.master.attributes("-topmost", True)
             self.master.attributes("-topmost", False)
-    
-    def on_closing(self):
-        if self.time_thread.is_alive():
-            self.run = False
-            self.time_thread.join()
-        self.master.destroy()
 
     def update_time(self, new_total):
         self.time = new_total
@@ -78,18 +95,6 @@ class Pomodoro:
     def update_entry(self):
         minutos, segundos = self.time_label.get().split(":")
         self.update_time(int(minutos) * 60 + int(segundos))
-
-    def focus(self):
-        self.update_time(25*60)
-        self.start()
-
-    def short_break(self):
-        self.update_time(5*60)
-        self.start()
-    
-    def long_break(self):
-        self.update_time(10*60)
-        self.start()
 
 
 def main(): 
